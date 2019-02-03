f6f54925a08fb229b68acbc1411120a293681488
admin <visualjupiter@gmail.com>
2019-02-03 15:54:55 +0900

    feat: scroll snapによるスライダーの実装

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,63 @@
 <template>
-  <HelloWorld />
+  <div class="PhotoList">
+    <div v-for="image in images" :key="image.id" class="PhotoList_item"><img :src="image.src"></div>
+  </div>
 </template>
 
 <script>
 import HelloWorld from '../components/HelloWorld.vue';
 
 export default {
+  data() {
+    return {
+      images: [
+        {
+          id: 0,
+          src: 'https://picsum.photos/400/300/?image=0',
+        },
+        {
+          id: 1,
+          src: 'https://picsum.photos/400/300/?image=10',
+        },
+        {
+          id: 2,
+          src: 'https://picsum.photos/400/300/?image=20',
+        },
+        {
+          id: 3,
+          src: 'https://picsum.photos/400/300/?image=30',
+        },
+        {
+          id: 4,
+          src: 'https://picsum.photos/400/300/?image=40',
+        },
+      ],
+    };
+  },
   components: {
     HelloWorld,
   },
 };
 </script>
+
+<style lang="scss" scoped>
+.PhotoList {
+  display: flex;
+  overflow-x: scroll;
+
+  scroll-snap-type: x mandatory;
+}
+.PhotoList_item {
+  flex-shrink: 0;
+  flex-grow: 0;
+  flex-basis: 100%;
+
+  scroll-snap-align: center;
+
+  img {
+    vertical-align: top;
+    width: 100%;
+  }
+}
+</style>
+
